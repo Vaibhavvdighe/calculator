e3cb274550c4ca0a4e695c319ad3c348f1828877
Vaibhav Dighe <90597458+Vaibhavvdighe@users.noreply.github.com>
2024-09-24 14:01:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-document.querySelectorAll('td');
 const display = document.getElementById('space');
 const calculator = document.querySelector('table');
 const back = document.getElementById('back');
+const td = document.querySelectorAll('td');
+
+const main = document.querySelector('main');
+const theme = document.querySelector('.theme');
+const footer = document.querySelector('footer');
+
+let currentTheme = 'dark';
+theme.addEventListener('click', ()=>{
+    changeTheme();
+})
+
+function changeTheme() {
+    if(currentTheme=='dark') {
+        currentTheme = 'light';
+        theme.innerHTML=`<i class="fa-solid fa-xl fa-moon"></i>`;
+        theme.style.backgroundColor = "white";
+        footer.style.backgroundColor = "rgb(207, 207, 207)";
+        footer.style.color = "#212121";
+        calculator.style.color="black";
+        calculator.style.backgroundColor="white";
+        document.body.style.backgroundColor = "white";
+    }
+    else if(currentTheme=='light') {
+        currentTheme = 'dark';
+        theme.innerHTML=`<i class="fa-solid fa-sun fa-xl" style="color: white;"></i>`;
+        theme.style.backgroundColor = "#212121";
+        footer.style.backgroundColor = "rgb(22, 22, 22)";
+        footer.style.color = "rgb(125, 124, 124)";
+        calculator.style.color="#ffffff";
+        calculator.style.backgroundColor="rgb(22, 22, 22)";
+        document.body.style.backgroundColor = "#212121";
+    }
+}
 
 
 let openBracket = true
